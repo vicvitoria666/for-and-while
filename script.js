d764ea8928f190ed3480c6cefd824410f08c493e
vicvitoria666 <https.vitoriaoliveira@gmail.com>
2022-02-16 15:18:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+ for(let num = 1; num <= 10; num++) {
+     console.log(num);
+ }
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+ 
+let num2 = 10
+while ( num2 >= 10) {
+    console.log (num2)
+    num2++;
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for (let num3 = 1; num3 <= 100; num3 += 2) {
+    console.log(num3);
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for (let num4 = 0; num4 <= 100; num4 += 2) {
+    console.log(num4);
+}
